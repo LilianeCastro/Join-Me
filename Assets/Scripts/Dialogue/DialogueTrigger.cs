b1792d214cf20eea29a515498bd6fb8ea9c4f536
Liliane Castro <lcs2@icomp.ufam.edu.br>
2021-02-03 14:10:50 -0400

    feat: tutorial level

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -7,7 +7,7 @@ public class DialogueTrigger : MonoBehaviour
     [SerializeField] private Dialogue _dialogue;
     [SerializeField] private bool _canDestroy = false;
     [SerializeField] private Color _colorText = default;
-    [SerializeField] [Range(1.0f, 5.0f)] private float _delayTimeInScene = 2.0f;
+    [SerializeField] [Range(0.5f, 5.0f)] private float _delayTimeInScene = 2.0f;
 
 
     private void OnTriggerEnter2D(Collider2D other)
